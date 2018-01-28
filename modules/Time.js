d9e97daf7b31817d8f442888af4591e775b4b3ee
Oskar Kobierecki <oskar.kobierecki@o2.pl>
2018-01-29 00:56:33 +0100

    Time works

--- modules/Time.js
+++ modules/Time.js
@@ -2,7 +2,7 @@
 function getConvertedTime(answer){
     if(answer !== null){
         var date = new Date(null);
-        date.setSeconds(input);
+        date.setSeconds(answer);
         var result = date.toISOString().substr(11,2) + ' hours ' + date.toISOString().substr(14,2) + ' minutes ' +  date.toISOString().substr(17,2) + ' seconds ';
         console.log(result);
     }
